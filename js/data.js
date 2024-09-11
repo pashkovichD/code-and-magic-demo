547402fcc346840ad1c34c1c71ddd2689f828102
pashkovichD <d.pashkovich@gmail.com>
2024-09-11 14:53:25 +0300

    Добавлена функция создания волшебника

---
+++ js/data.js
@@ -0,0 +1,72 @@
+/*
+    Объект состоит из трех ключей:
+    name, строка - случайно сгенерированное имя персонажа.
+    Имя генерируется из массива имен и фамилий: нужно случайным образом выбрать
+    из массива имен имя, а из массива фамилий фамилию и сложить их.
+
+    coatColor, строка - случайный цвет мантии на выбор.
+
+    eyesColor, строка - случайный цвет глаз персонажда на выбор.
+*/
+
+const NAMES = [
+    'Иван',    
+    'Хуан Себастьян',    
+    'Мария',    
+    'Кристоф',    
+    'Виктор',    
+    'Юлия',    
+    'Лолита',    
+    'Вашингтон'    
+];
+
+const SURNAMES = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+];
+
+const COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(243, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+];
+
+const EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+];
+
+const getRandomPositiveInteger = (a, b) => {
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+    const result = Math.random() * (upper - lower + 1) + lower;
+    return Math.floor(result);
+};
+
+/* Функция, которая создает волшебника */
+const createWizard = () => {
+    const randomNameIndex = getRandomPositiveInteger(0, NAMES.length - 1);
+    const randomSurnameIndex = getRandomPositiveInteger(0, SURNAMES.length - 1);
+    const randomCoatColorIndex = getRandomPositiveInteger(0, COAT_COLORS.length - 1);
+    const randomEyesColorIndex = getRandomPositiveInteger(0, EYES_COLORS.length - 1);
+    
+    return {
+        name: NAMES[randomNameIndex] + ' ' + SURNAMES[randomSurnameIndex],
+        coatColor: COAT_COLORS[randomCoatColorIndex],
+        eyesColor: EYES_COLORS[randomEyesColorIndex]
+    };
+};
+
+console.log(createWizard());
---
+++ js/setup.js
@@ -0,0 +1,59 @@
+import startGame from './game.js';
+// ^ не обращайте внимания на эту строчку,
+// о подключении одних JS-файлов внутри других
+// мы поговорим совсем скоро
+
+
+// С помощью JavaScript'а настраивается поведение игры:
+
+// - размер фаербола
+const FIREBALL_SIZE = 22;
+// - ширина мага
+const WIZARD_WIDTH = 70;
+// - скорость мага
+const WIZARD_SPEED = 3;
+
+// // - скорость движение фаербола (вчсл)
+// function getFireballSpeed(isMovingLeft) {
+//     /* if(isMovingLeft) {
+//         return 2;
+//     }
+//     return 5; */
+    
+//     // ...или лучше с помощью тернарного оператора
+//     return isMovingLeft ? 2 : 5;
+// }
+
+// // - высота мага (вчсл)
+// function getWizardHeight() {
+//     return 1.337 * WIZARD_WIDTH;
+// }
+
+// // - начальное положение на игровом поле (вчсл)
+// function getWizardX(gameFieldWidth) {
+//     return (gameFieldWidth - WIZARD_WIDTH) / 2;
+// }
+// function getWizardY(gameFieldHeight) {
+//     return gameFieldHeight / 3;
+// }
+
+/* Переписали функциии с Declaration на стрелочные */
+
+const getFireballSpeed = (isMovingLeft) => isMovingLeft ? 2 : 5;
+
+const getWizardHeight = () => 1.337 * WIZARD_WIDTH;
+
+const getWizardX = (gameFieldWidth) => (gameFieldWidth - WIZARD_WIDTH) / 2;
+
+const getWizardY = (gameFieldHeight) => gameFieldHeight / 3;
+
+// Поведение игры
+startGame(
+    FIREBALL_SIZE,
+    getFireballSpeed,
+    WIZARD_WIDTH,
+    WIZARD_SPEED,
+    getWizardHeight,
+    getWizardX,
+    getWizardY
+);
